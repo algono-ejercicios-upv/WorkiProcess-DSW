a2be91f18afc8209c357bdf941471afa9fdc2a79
algono <algono@inf.upv.es>
2020-05-01 15:01:53 +0000

    Update dependencies

--- git-push.sh
+++ git-push.sh
@@ -1,3 +1,7 @@
+# On any failed command, exit the program with an error message
+set -e
+trap "The program failed. Exiting..." ERR
+
 DATABASE_USER="drupal1"
 DATABASE_NAME="drupal1"
 DATABASE_BACKUP="./mis-backups/database_backup.sql"
