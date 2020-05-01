f9085b54efee9cd43e16c26fbc310b0f6464e599
algono <algono@inf.upv.es>
2020-05-01 14:24:35 +0000

    Database backup script

---
+++ git-push.sh
@@ -0,0 +1,13 @@
+
+DATABASE_USER="drupal1"
+DATABASE_NAME="drupal1"
+DATABASE_BACKUP="./mis-backups/database_backup.sql"
+
+rm -v "$DATABASE_BACKUP"
+mysqldump -u "$DATABASE_USER" -p "$DATABASE_NAME" > "$DATABASE_BACKUP"
+
+echo "Commit message: $1"
+
+git add .
+git commit -m "$2"
+git push
